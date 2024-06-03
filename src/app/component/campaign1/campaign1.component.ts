16e40052d7cb81082539aa38f9dafa57b103b7e4
first-Curious <jagtaprathesh1819@gmail.com>
2024-06-03 17:24:39 +0530

    first commit

---
+++ src/app/component/campaign1/campaign1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign1',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaign1.component.html',
+  styleUrl: './campaign1.component.css'
+})
+export class Campaign1Component {
+
+}
---
+++ src/app/component/campaign2/campaign2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign2',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaign2.component.html',
+  styleUrl: './campaign2.component.css'
+})
+export class Campaign2Component {
+
+}
